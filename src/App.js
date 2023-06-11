95c7e0138ce2aeb59784b5f87a43c590914df7aa
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-06-11 12:38:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // https://dummyjson.com/products
+  const [product , setProduct] = useState([]);
+  const [page,setPage] = useState(1);
+
+  ///solution 2
+
+  const [totalpage,setTotalPages] = useState(0);
+
+  const fetchData = async () => {
+    const res = await fetch('https://dummyjson.com/products?limit=100');
+    //https://dummyjson.com/products?limit=10&skip=$(page*10-10)"
+    const data = await res.json();
+    if(data && data.products){
+      setProduct(data.products);
+      setTotalPages(data.total/10);
+
+    }
+      //  console.log(data.products);
+  }
+  useEffect(()=> {
+    fetchData()
+  },[])  //[page]
+
+  const selectPageHandler= (selectedPage) =>{
+    if(selectedPage >= 1 && selectedPage <= 10 && selectedPage !== page){
+      setPage(selectedPage);
+    }
+   
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <h1 style={{textAlign:'center'}}>Product List</h1><br/>
+      {
+        product.length>0 && 
+        <div className='pagination'>
+        <span 
+        onClick={() => selectPageHandler(page-1)}
+        className={page > 1 ? "":"pagination__disable"}
+        ><big>⬅️</big></span>
+        {
+          [...Array(product.length/10)].map((_,i)=>(
+            <span 
+            className={page === i+1 ? "activePage":""}
+            onClick={()=>selectPageHandler(i+1)} 
+            key={i}> {i+1} </span>
+          ))
+        } 
+        <span 
+        onClick={() => selectPageHandler(page+1)}
+        className={page < product.length/10 ? "":"pagination__disable"}
+        ><big>➡️</big></span>
+      </div>
+      }
+      {
+        product.length > 0 && <div className='product'>
+         {
+           product.slice(page * 10 - 10 ,page * 10).map((prod)=>(
+            <div className='card' key={prod.id}>
+              <img src={prod.thumbnail} alt={prod.title}/>
+              <p>{prod.title}</p>
+            </div>
+          ))
+         }
+        </div>
+      }
     </div>
   );
 }
